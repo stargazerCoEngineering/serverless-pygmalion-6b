a76b5b3b7135881970bd77003bfae37d7c4da938
stargazerCoEngineering <134083445+stargazerCoEngineering@users.noreply.github.com>
2023-05-19 16:44:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def init():
         model.cuda()
         print("done")
 
-    tokenizer = GPT2Tokenizer.from_pretrained("mayaeary/pygmalion-6b_dev-4bit-128g")
+    tokenizer = GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
 
 
 # Inference is ran for every server call
